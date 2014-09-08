fd864f70ab219730485e72505150319ae47ec970
Kenny11CZ <dan.balarin@gmail.com>
2014-09-08 13:29:07 +0200

    Vstup/Vystup

--- Prvni_hodina/main.cpp
+++ Prvni_hodina/main.cpp
@@ -2,11 +2,17 @@
 
 int main(int argc, char *argv[])
 {
+    //Promene
     int cele_cislo = 15;
-    char znak = 't';
+    char znak;
     float pi = 3.14;
+    //Vstup
+    printf("Zadejte znak: ");
+    scanf("%c", &znak);
+    //Vystup
     printf("Char je: %c\n", znak);
     printf("Pi je: %f\n", pi);
     printf("Cele cislo je: %d\n\n", cele_cislo);
+    //Konec
     return 0;
 }
