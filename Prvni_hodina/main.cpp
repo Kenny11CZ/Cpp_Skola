1ea77da2a501707d0b591e8aa6b1263d47459227
Kenny11CZ <dan.balarin@gmail.com>
2014-09-08 13:16:47 +0200

    Hello world commit
    yeeeey

--- Prvni_hodina/main.cpp
+++ Prvni_hodina/main.cpp
@@ -1,8 +1,12 @@
-#include <QCoreApplication>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+    int cele_cislo = 15;
+    char znak = 't';
+    float pi = 3.14;
+    printf("Char je: %c\n", znak);
+    printf("Pi je: %f\n", pi);
+    printf("Cele cislo je: %d\n\n", cele_cislo);
+    return 0;
 }
