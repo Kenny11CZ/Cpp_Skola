030735fc217ef11ba10e5f8f977972dee5d17a99
Kenny11CZ <dan.balarin@gmail.com>
2014-09-09 10:29:01 +0200

    Linearni rovnice ax+b=0

--- Druha_hodina/main.cpp
+++ Druha_hodina/main.cpp
@@ -2,10 +2,24 @@
 
 int main(int argc, char *argv[])
 {
+    /*
+    //Promenne
     int a;
-    scanf("%d", a);
+    //Vstup
+    scanf("%d", &a);
+    //Podminka pokud a je vetsi nez 0 tj. kladne
     if(a > 0){
         printf("Some shit\n");
     }
+    */
+
+    float a,b,x;
+    printf("Vitejte v kalkulacce linearnich rovnic.\n\nTvar rovnice:ax+b=0\n");
+    printf("A: ");
+    scanf("%f", &a);
+    printf("B: ");
+    scanf("%f", &b);
+    x=(0-b)/2;
+    printf("Vysledek je: %f\n",x);
     return 0;
 }
