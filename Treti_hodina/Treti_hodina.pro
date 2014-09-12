70681e34322f86e2933c395af0a34d771b69d5a9
Kenny11CZ <dan.balarin@gmail.com>
2014-09-12 10:31:47 +0200

    Kvadraticka kalkulacka

---
+++ Treti_hodina/Treti_hodina.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-09-12T10:12:10
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Treti_hodina
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ Treti_hodina/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    /* Kalkulacka kvadratickzch rovnic
+    float a,b,c,x;
+    printf("Vitejte v me kalkulacce\nPokracejte zadanim rovnice ve tvaru ax+b=c");
+    printf("\nA: ");
+    scanf("%f", &a);
+    printf("B: ");
+    scanf("%f", &b);
+    printf("C: ");
+    scanf("%f", &c);
+    if(a==0){
+        if(b==c){
+            printf("Nekonecno\n");
+        }
+        else printf("\nVysledek je: 0\n");
+    }
+    else{
+        x=(c-b)/a;
+        printf("\n\n%.1f * %.1f + %.1f = %.2f", a,x,b,c);
+        printf("\nVysledek je: %.2f\n", x);
+    }*/
+    return 0;
+}
